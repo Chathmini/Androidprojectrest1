252d0b1a160ff4d0cd307afcde5823dfca91e14e
Chathmini <chathmini96@gmail.com>
2020-01-23 20:59:23 +0530

    Android test project

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun click (view: View){
+        var intent1 = Intent(this,Main2Activity::class.java)
+        startActivity(intent1)
+    }
+}
